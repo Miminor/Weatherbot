8427a690767a2b0b9e6bb152c130adfdda335b4a
Miminor <86786284+Miminor@users.noreply.github.com>
2021-07-07 18:57:32 +0300

    Add files via upload

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -1,9 +1,11 @@
+import asyncio
+import json
+
 from config import tg_bot_token
 from config import open_weather_token
-import schedule
-import time
 
 import requests
+import logging
 from datetime import datetime
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -12,12 +14,26 @@ from aiogram.utils import executor
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
+logging.basicConfig(level=logging.INFO)
+logs = logging.getLogger('broadcast')
+
+with open("user_database.json", encoding='utf-8') as file:
+    users_dict = json.load(file)
+
 
 def log(message):
     print("<!------!>")
     print(datetime.now())
     print("Сообщение от {0} {1} id = {2} \n {3}".format(message.from_user.first_name, message.from_user.last_name,
                                                         str(message.from_user.id), message.text))
+    user_id = message.from_user.id
+
+    users_dict[user_id] = {
+        "user": message.from_user.as_json()
+    }
+
+    with open("user_database.json", "w", encoding='utf-8') as users:
+        json.dump(users_dict, users, ensure_ascii=False)
 
 
 @dp.message_handler(commands=["id"])
@@ -31,21 +47,41 @@ async def usr_help(message):
     await message.reply(
         "Просто введите название города на любом языке и бот выдаст сводку погоды на текущий момент \U0001F31D\n"
         "Также с помощью команды /id вы можете узнать свой id в телеграмме")
-    log(message)
 
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.reply("Привет! Напиши мне название города и я пришлю тебе сводку погоды!")
-    log(message)
 
-@dp.message_handler(commands=["queque"])
-async def job(message: types.Message):
-    await message.reply("Введите название города")
-    schedule.every(10).minutes.do(get_weather)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+
+foo_counter = 0
+
+
+async def insult(message: types.Message):
+    r = requests.get("https://evilinsult.com/generate_insult.php?lang=en&type=json")
+    data = r.json()
+    insult_for_you = data["insult"]
+    await message.answer(insult_for_you)
+
+
+@dp.message_handler(commands=["once"])
+async def once_a_day(message: types.Message):
+    arguments = message.get_args()
+    global foo_counter
+    if not arguments:
+        if foo_counter % 2 == 0:
+            await message.answer("Для получения ежедневной сводки в это же время дня отправьте команду в формату "
+                                 "/once Киев")
+        elif foo_counter % 2 != 0:
+            foo_counter += 1
+            await message.answer("Ежедневная сводка остановлена")
+    elif foo_counter % 2 == 0:
+        foo_counter += 1
+        await message.answer("Для отмены введите команду /once еще раз")
+        while foo_counter % 2 != 0:
+            await asyncio.sleep(3600)
+            await get_weather(message)
+
 
 @dp.message_handler()
 async def get_weather(message: types.Message):
@@ -67,9 +103,17 @@ async def get_weather(message: types.Message):
         5: "Очень высокий \U00002620"
     }
 
+    arg = message.get_args()
+
+    if arg is None:
+        city_name = message.text
+    else:
+        city_name = arg
+
     try:
         r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric&lang=ua, uk"
+            f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={open_weather_token}&units=metric"
+            f"&lang=ua, uk "
         )
         data = r.json()
 
@@ -77,7 +121,8 @@ async def get_weather(message: types.Message):
         location_lon = data["coord"]["lon"]
 
         r2 = requests.get(
-            f"http://api.openweathermap.org/data/2.5/air_pollution?lat={location_lat}&lon={location_lon}&appid={open_weather_token}"
+            f"http://api.openweathermap.org/data/2.5/air_pollution?lat={location_lat}&lon={location_lon}"
+            f"&appid={open_weather_token} "
         )
 
         air = r2.json()
@@ -87,8 +132,8 @@ async def get_weather(message: types.Message):
         city = data["name"]
         cur_humidity = data["main"]["humidity"]
         cur_temp = data["main"]["temp"]
-        #max_temp = data["main"]["temp_max"]
-        #min_temp = data["main"]["temp_min"]
+        # max_temp = data["main"]["temp_max"]
+        # min_temp = data["main"]["temp_min"]
         wind = data["wind"]["speed"]
         sunrise_timestamp = datetime.fromtimestamp(data["sys"]["sunrise"])
         sunset_timestamp = datetime.fromtimestamp(data["sys"]["sunset"])
@@ -116,5 +161,6 @@ async def get_weather(message: types.Message):
 
     log(message)
 
+
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
